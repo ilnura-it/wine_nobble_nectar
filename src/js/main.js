b1ee96fce561b17e99da7ac3b02b679a2805b294
Ilnura <t.ilnura@gmail.com>
2022-08-10 20:56:16 -0600

    finished project

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,24 @@ window.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         loop: true,
-        centeredSlides: true,
-        slidesPerView: 1.4,
-        spaceBetween: 30,
-        initialSlide: 2,
         autoplay: {
-            delay: 2000
+            delay: 2000,
+            disabldeOnInteraction: false
+        },
+        breakpoints: {
+          // when window width is >= 320px
+          320: {
+                slidesPerView: 1.1,
+                spaceBetween: 20,
+                centeredSlides: false
+          },
+          // when window width is >= 480px
+          450: {
+                slidesPerView: 1.4,
+                spaceBetween: 30,
+                centeredSlides: true,
+                initialSlide: 1
+          }
         }
       });
 })
